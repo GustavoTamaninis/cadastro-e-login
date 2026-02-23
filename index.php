2a3259174ba3a7da4d70106c80f8613d0d007d60
GustavoTamaninis <gustavotamanini2005@gmail.com>
2026-02-23 14:45:56 -0300

    feat: esqueleto da página index criado.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Site</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+<body class="bg-dark text-light">
+    <div class="row mt-5">
+        <div class="col-md-6 offset-3 p-3 text-center bg-black">
+        </div>
+    </div>
+    <script src="/assets/css/bootstrap.min.css"></script>
+</body>
+</html>
