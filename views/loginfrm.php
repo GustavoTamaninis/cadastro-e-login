703d04150b64378679383b90d8edbab0f72df2f7
GustavoTamaninis <gustavotamanini2005@gmail.com>
2026-02-24 12:13:48 -0300

    feat: formulário de login e sua estilização básica

---
+++ views/loginfrm.php
@@ -0,0 +1,27 @@
+<form action="#" method="post" class="">
+    <div class="row mb-3">
+        <div class="col">
+            <h2>Entrar</h2>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-6 offset-3 mb-3">
+            <label for="txt_email" class="form-label">E-mail:</label>
+            <input type="text" name="txt_email" id="txt_email" class="form-control">
+        </div>
+    <div>
+    <div class="form-row">
+        <div class="col-6 offset-3 mb-3">
+            <label for="txt_email" class="form-label">Senha:</label>
+            <input type="text" name="txt_email" id="txt_email" class="form-control">
+        </div>
+    <div>
+    <div class="form-row d-flex mt-4">
+        <div class="col-2 offset-4 mb-3">
+            <button type="reset" class="btn btn-outline-primary">Limpar</button>
+        </div>
+        <div class="col-2 mb-3">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+    </div>
+</form>
