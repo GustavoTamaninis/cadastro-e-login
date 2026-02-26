81faeeaa4d6ccb4c1b17f8cb00bbbd054cdafa63
GustavoTamaninis <gustavotamanini2005@gmail.com>
2026-02-26 13:13:58 -0300

    fix: ID's errados; css inexistente aplicado; alerta do bootstrap; feat: formulário espera o retorno da função validarLogin()

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Site</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/estilo.css">
 </head>
 <body class="bg-dark text-light">
     <div class="row mt-5">
