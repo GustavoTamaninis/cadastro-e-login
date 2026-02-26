6cefdb3b0506dfb4d139fcf61bb7f87adee02d10
GustavoTamaninis <gustavotamanini2005@gmail.com>
2026-02-26 13:14:47 -0300

    validação do login

--- assets/js/validaform.js
+++ assets/js/validaform.js
@@ -1,18 +1,25 @@
-const btnLogin = document.getElementById('btn_login');
-const btnRegistro = document.getElementById('btn_registro');
-
-document.addEventListener("DOMContentLoaded", function(){
+function validarLogin(){
     const frmLogin = document.getElementById('frmlogin');
-    const frmRegistro = document.getElementById('frmregistro');
+    const btnLogin = document.getElementById('btn_login');
 
-    if(frmLogin){
-        let cxEmail = document.getElementById('txt_email');
-        let cxSenha = document.getElementById('txt_senha');
-        alert(cxEmail.value);
-        alert(cxSenha.value);
-    }else if(frmRegistro){
+    const cxEmail = document.getElementById('txt_email');
+    const cxSenha = document.getElementById('txt_senha');
+    const email = cxEmail.value.trim() ?? "";
+    const senha = cxSenha.value.trim() ?? "";
 
+    console.log(email);
+    console.log(senha);
+
+    regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&-/])[A-Za-z\d@$!%*?&-/]{8,16}$/;
+
+    if(!regexEmail.test(email)){
+        alert("Preencha corretamente o campo de email.");
+        return false;
+    }else if(!regexSenha.test(senha)){
+        alert("Preencha corretamente o campo de senha.");
+        return false;
+    }else{
+        return true;
     }
-
-    
-})
+}
